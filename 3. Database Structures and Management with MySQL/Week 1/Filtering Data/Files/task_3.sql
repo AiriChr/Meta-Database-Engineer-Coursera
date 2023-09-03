b50db64b1c259c5de929dbe7ad4b82dcc52b0ccc
AC <airishchristian@gmail.com>
2023-09-03 08:40:27 +0800

    Added relevant files

---
+++ 3. Database Structures and Management with MySQL/Week 1/Filtering Data/Files/task_3.sql
@@ -0,0 +1,7 @@
+-- Task 3: Write a SQL statement to print all records of orders that have 
+-- been placed by the client with the id of Cl3 and where the cost of the 
+-- order is more than $100. The expected output result should be the same 
+-- as the following screenshot (assuming that you have populated the Orders
+--  table with the same data set)
+
+SELECT * FROM Orders WHERE ClientID = 'Cl3' and Cost > 100;
