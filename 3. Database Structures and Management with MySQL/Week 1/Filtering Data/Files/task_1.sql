8c1151fe954b30269b332d22b3b15d46684d667e
AC <airishchristian@gmail.com>
2023-09-03 08:39:16 +0800

    deleted files

--- 3. Database Structures and Management with MySQL/Week 1/Filtering Data/Files/task_1.sql
+++
@@ -1,6 +0,0 @@
--- Task 1: Write a SQL statement to print all records of orders where 
--- the cost is $250 or less. The expected output result should be the 
--- same as the following screenshot (assuming that you have populated 
--- the Orders table with the same data set)
-
-SELECT * FROM Orders WHERE Cost <= 250;
